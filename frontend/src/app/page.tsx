642cec4bb7fdd7f1d455a5c6bfe602a16778d16d
Jobayer Ahmed <jobayerahmed.dev@gmail.com>
2025-11-24 01:58:33 +0600

    shadcn instaled and src folder added

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default HomePage;
