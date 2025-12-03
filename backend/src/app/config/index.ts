5488bd589c95353697ae570593e8ebab3196d9f0
Jobayer Ahmed <jobayerahmed.dev@gmail.com>
2025-12-04 00:12:16 +0600

    establish database connection in server setup

--- backend/src/app/config/index.ts
+++ backend/src/app/config/index.ts
@@ -5,4 +5,5 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export default {
   port: process.env.PORT,
+  databaseUrl: process.env.DATABASE_URL,
 };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import { Server } from "http";
 import app from "./app";
 import config from "./app/config";
+import mongoose from "mongoose";
 
 let server: Server;
 
 async function main() {
   try {
+    await mongoose.connect(config.databaseUrl as string);
+    console.log("Connected to the database");
     server = app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
