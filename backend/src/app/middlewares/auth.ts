b0868ca406ec7347bd812db79945cbd10334d94d
Jobayer Ahmed <jobayerahmed.dev@gmail.com>
2025-12-08 22:48:39 +0600

    Added rquest validation middleware

--- backend/src/app/middlewares/auth.ts
+++ backend/src/app/middlewares/auth.ts
@@ -10,7 +10,7 @@ import AppError from '../errors/AppErrors';
 const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(' ')[1];
-    const secret = config.jwt_access_secret;
+    const secret = config.jwt_access_secret; 
     // console.log('token', token);
     // console.log('secret', secret);
     // console.log('body', req.body);
